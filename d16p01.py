d131c935995c8e59ea093b3ad509687fa85853ad
labadmin <labadmin@labadmins-MacBook-Pro.local>
2022-12-24 13:16:13 -0600

    day 24 solved

--- d16p01.py
+++ d16p01.py
@@ -1,46 +1,39 @@
 import re
-from itertools import permutations
+import functools
 
-f =  open('d16i01.txt','r')
+f =  open('d16s01.txt','r')
 lines = f.readlines()
 
-def paths_to_valves(valves, cur, rem_min):
-    valves = dict(sorted(valves.items(), key=lambda item: item[1]["flow"], reverse=True))
-    possible_targets = []
-    for valve in valves:
-        if valves[valve]["flow"] > 0 and valves[valve]["state"] == 0:
-            possible_targets.append((valve, valves[valve]["flow"]))
+def get_states(valves, states):
+    new_states = []
+    for state in states:
+        cur = state['cur']
+        neighbors = valves[cur]['neighbors']
+        if valves[cur]['flow'] > 0 and cur not in state['opens']:
+            new_states.append({'cur': cur, 
+                               'flow': state['flow'] + valves[cur]['flow'],
+                               'opens': state['opens'] + [cur],
+                               'rem_min': state['rem_min'] - 1,
+                               'released': state['released'] + state['flow']})
+        for neighbor in neighbors:
+            new_states.append({'cur': neighbor,
+                               'flow': state['flow'],
+                               'opens': state['opens'],
+                               'rem_min': state['rem_min'] - 1,
+                               'released': state['released'] + state['flow']})
+    return new_states
 
-    possible_targets = sorted(possible_targets, key = lambda x: x[1], reverse=True)
+#def paths_to_valves(valves, cur, rem_min):
+#    valves = dict(sorted(valves.items(), key=lambda item: item[1]["flow"], reverse=True))
+#    possible_targets = []
+#    for valve in valves:
+#        if valves[valve]["flow"] > 0 and valves[valve]["state"] == 0:
+#            possible_targets.append((valve, valves[valve]["flow"]))
+#
+#    possible_targets = sorted(possible_targets, key = lambda x: x[1], reverse=True)
+#    return (cur, 0, [cur])
 
-    if len(possible_targets) > 0:
-        highest_value = 0
-        winning_opt = None
-        opts = list(permutations(range(min(4, len(possible_targets)))))
-        for o in opts:
-            pts = []
-            for p in range(len(o)):
-                pts.append(possible_targets[o[p]][0])
-            paths = [shortest_path(valves, cur, pts[0])]
-            for p in range(len(o)-1):
-                paths.append(shortest_path(valves, pts[p], pts[p+1]))
-            values = []
-            for p in range(len(o)):
-                rm = rem_min
-                for v in range(p+1):
-                    rm = rm - len(paths[v])
-                values.append(max(0, rm * possible_targets[o[p]][1]))
-            value = sum(values)
-
-            if value > highest_value:
-                    highest_value = value
-                    winning_opt = o
-        #print((winning_opt, possible_targets))
-
-        return (possible_targets[winning_opt[0]][0], possible_targets[winning_opt[0]][1], shortest_path(valves, cur, possible_targets[winning_opt[0]][0]))
-    else:
-        return (cur, 0, [cur])
-
+@functools.lru_cache()
 def shortest_path(valves, start, target):
     unvisited = []
     visited = []
@@ -92,18 +85,10 @@ current_valve = "AA"
 flows = []
 pressure = 0
 
+states = [{'cur': 'AA', 'flow': 0, 'opens': [], 'rem_min': 30, 'released': 0}]
 for minute in range(30):
-    print((minute, current_valve, flows))
+    print(minute)
+    states = get_states(valves, states)
 
-    for flow in flows:
-        pressure = pressure + flow
-
-    next_valve = paths_to_valves(valves, current_valve, 30 - minute)
-    #print(next_valve)
-    if next_valve and next_valve[0] != current_valve:
-        current_valve = next_valve[2][0]
-    elif valves[current_valve]["state"] == 0:
-        valves[current_valve]["state"] = 1
-        flows.append(valves[current_valve]["flow"])
-
-    print(pressure)
+for state in states:
+    print(state['released'])
---
+++ d24p02.py
@@ -0,0 +1,160 @@
+f = open("d24i01.txt")
+lines = f.readlines()
+
+start = complex(0, lines[0].index('.'))
+end = complex(len(lines) - 1, lines[-1].index('.'))
+
+blizzes = {'>': set(), '<': set(), '^': set(), 'v': set()}
+move_ops = {'>': 1j, 'v': 1 + 0j, '^': -1 + 0j, '<': -1j}
+#move_ops = {'^': -1 + 0j, '<': -1j, '>': 1j, 'v': 1 + 0j}
+
+for i in range(1, len(lines) - 1):
+    for k in range(1, len(lines[i].strip()) -1):
+        if lines[i][k] != '.':
+            blizzes[lines[i][k]].add(complex(i, k))
+
+def print_map():
+    print(" ", end="")
+    for c in range(len(lines[0].strip())):
+        print(c % 10, end="")
+    print()
+    for r in range(len(lines)):
+        print(r % 10, end="")
+        for c in range(len(lines[0].strip())):
+            if complex(r, c) == pos:
+                print("E", end="")
+            elif r == 0 or r == len(lines) - 1:
+                if complex(r, c) == start \
+                  or complex(r, c) == end:
+                    print(".", end="")
+                else:
+                    print("#", end="")
+            elif c == 0 or c == end.imag + 1:
+                print("#", end="")
+            else:
+                bliz_found = []
+                for d in ['<', '>', '^', 'v']:
+                    if complex(r, c) in blizzes[d]:
+                        bliz_found.append(d)
+                if len(bliz_found) == 0:
+                    print(' ', end="")
+                elif len(bliz_found) == 1:
+                    print(bliz_found[0], end="")
+                else:
+                    print(len(bliz_found), end="")
+        print()
+
+def move_bliz(blizzes):
+    new_blizzes = {'>': set(), '<': set(), '^': set(), 'v': set()}
+    for d in move_ops:
+        for bliz in blizzes[d]:
+            bliz = bliz + move_ops[d]
+            if bliz.imag < 1:
+                bliz = complex(bliz.real, end.imag)
+            elif bliz.imag > end.imag:
+                bliz = complex(bliz.real, 1)
+            elif bliz.real < 1:
+                bliz = complex(end.real -1, bliz.imag)
+            elif bliz.real == end.real:
+                bliz = complex(1, bliz.imag)
+            new_blizzes[d].add(bliz)
+    return new_blizzes
+
+def open_neighbors(blizzes, pos):
+    open_spots = []
+    for dir in move_ops:
+        spot = pos + move_ops[dir]
+        #if spot == start \
+        if spot == end \
+                or (spot.real > 0 and spot.real < end.real \
+                and spot.imag > 0 and spot.imag <= end.imag):
+            found = False
+            for d2 in move_ops:
+                if spot in blizzes[d2]:
+                    found = True
+            if not found:
+                open_spots.append(spot)
+    found = False
+    for d2 in move_ops:
+        if pos in blizzes[d2]:
+            found = True
+    if not found:
+        open_spots.append(pos)
+    return open_spots
+
+def revert_save():
+    global c
+    global pos
+    global saves
+    global blizzes
+    global win
+    if len(saves) > 0:
+        save = [10001, 0, 0, 0]
+
+        while save[0] > win:
+            if (save[0], save[3]) in done_saves:
+                save = saves.pop()
+            else:
+                save = saves.pop()
+                done_saves.add((save[0], save[3]))
+        c = save[0]
+        blizzes = save[1]
+        pos = save[2][0]
+        if len(save[2]) > 1:
+            saves.append((c, blizzes, save[2][1:], pos))
+        else:
+            done_saves.add((c, pos)) 
+
+for _ in range(305):
+    blizzes = move_bliz(blizzes)
+
+for _ in range(289):
+    blizzes = move_bliz(blizzes)
+
+saves = []
+done_saves = set()
+done = False
+#pos = end
+pos = start
+print_map()
+win = 10000
+c = 0
+while not done:
+    print(c, win, len(saves), len(done_saves))
+    for save in saves:
+        if save[0] >= win:
+            saves.remove(save)
+            done_saves.add((save[0], save[3]))
+        elif (save[0], save[3]) in done_saves:
+            saves.remove(save)
+    if pos == end:
+    #if pos == start:
+        win = min(win, c)
+        if len(saves) > 0:
+            revert_save()
+        else:
+            print(win)
+            done = True
+            exit()
+    if c > win:
+        revert_save()
+    #print_map()
+    #print()
+    c += 1
+    blizzes = move_bliz(blizzes)
+    opens = open_neighbors(blizzes, pos)
+    if len(opens) == 1:
+        pos = opens[0]
+    elif len(opens) > 1:
+        if (c, pos) in done_saves:
+            revert_save()
+        else:
+            saves.append((c, blizzes, opens[1:], pos))
+            pos = opens[0]
+    elif len(opens) == 0:
+        revert_save()
+
+
+#Part 1: 305
+#Part 2a: 289
+#Part 2b: 311 
